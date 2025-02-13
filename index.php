1c45ef57cfb4760d7d35b978453ecab59c6bce29
RAZ <R.zaharia@anteq.fr>
2025-02-13 22:22:52 +0100

    Essai1

---
+++ index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <title>The ArtBox</title>
+</head>
+
+<?php $oeuvres = include 'oeuvres.php'; ?>
+
+<body>
+    
+    <?php include 'header.php'; ?>
+    
+    <main>
+    <div id="liste-oeuvres">
+            <?php foreach ($oeuvres as $oeuvre): ?>
+                <article class="oeuvre">
+                    <a href="oeuvre.php?id=<?= $oeuvre['id']; ?>">
+                        <img src="<?= $oeuvre['image']; ?>" alt="<?= $oeuvre['titre']; ?>">
+                        <h2><?= $oeuvre['titre']; ?></h2>
+                        <p class="description"><?= $oeuvre['auteur']; ?></p>
+                    </a>
+                </article>
+            <?php endforeach; ?>
+        </div>
+    </main>
+    
+    <?php include 'footer.php'; ?>
+    
+</body>
+</html>
